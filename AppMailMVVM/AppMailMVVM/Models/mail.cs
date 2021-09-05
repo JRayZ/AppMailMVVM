2e7ebc52110f09284f7278c3bfcc7f41f4690398
JHONATAN RAYMOND SANCHEZ DIAZ <1076689@est.intec.edu.do>
2021-09-04 20:08:20 -0400

    adding models

---
+++ AppMailMVVM/AppMailMVVM/Models/mail.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppMailMVVM.Models
+{
+    public class mail
+    {
+        public mail(string title, string description, string from, string to, string image)
+        {
+            Title = title;
+            Description = description;
+            From = from;
+            To = to;
+            date = DateTime.Now;
+            Image = image;
+        }
+
+        public string Title { get; set; }
+        public string Description {get; set;}
+        public string From { get; set; }
+        public string To { get; set; }
+        public DateTime date { get; set; }
+        public string Image { get; set; }
+    }
+}
